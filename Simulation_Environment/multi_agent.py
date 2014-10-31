ef83d8e1c69911deb6e80a53fc0c7f388775a6a5
tdm5923 <tdm5923@rit.edu>
2014-10-31 17:57:47 -0400

    Added in base terrain
    the terrain is standard now

--- Simulation_Environment/multi_agent.py
+++ Simulation_Environment/multi_agent.py
@@ -53,10 +53,11 @@ environment = environment.Environment(agent_behavior_function, dt = .05, sim_tim
 	
 environment.create_perimeter_walls(location = 'inside', thickness = 5)
 # Create terrain
-#create.create_random_terrain(environment, count = 30, scale = 60)
+#create.create_random_terrain(environment, count = 1, scale = 60)
 # Create instances of agents
+create.create_test_terrain(environment,60)
 create.create_random_swarm(environment, count = 10, radius = 20, velocity_range = (150, 300))
-#create.create_terrain_test_agents(environment)
+#
 
 # Create active action manager
 active_actions = active_actions.ActiveActions(environment)
